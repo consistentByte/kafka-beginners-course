8ff13457bfb78ad05c8b83aed35727804532632d
Saurabh Pandey <consistentByte@gmail.com>
2026-02-16 12:31:34 +0530

    Initial commit

---
+++ kafka-basics/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+//    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+//    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+// Source: https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:4.1.0")
+    // Source: https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:1.7.36")
+// Source: https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    testImplementation("org.slf4j:slf4j-simple:2.0.17")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,8 @@
+package io.conduktor.demos.kafka;
+
+public class ProducerDemo {
+    static void main() {
+        System.out.println("Hello World!");
+
+    }
+}
