b14db628ffc66abf92a95f884d9cf3dd441536c2
Saurabh Pandey <consistentByte@gmail.com>
2026-02-17 09:33:40 +0530

    OpenSearch Consumer, Advanced Consumer Configs, Project setup

---
+++ kafka-consumer-opensearch/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.opensearch.client:opensearch-rest-high-level-client:3.5.0")
+    // Source: https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    implementation("org.apache.kafka:kafka-clients:4.1.0")
+    // Source: https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:1.7.36")
+// Source: https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:1.7.36")
+//    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+//    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "kafka-beginners-course"
 //include("kafka-basics:kafka-producer-wikimedia")
 include("kafka-producer-wikimedia")
+include("kafka-consumer-opensearch")
